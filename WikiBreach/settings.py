8e888c0ce22e7f3904c7e8db89bb8b76a6058fb8
aditi-sharma <aditisharma.b@gmail.com>
2017-05-18 00:00:49 -0400

    Added global variable for site url

--- WikiBreach/settings.py
+++ WikiBreach/settings.py
@@ -60,6 +60,7 @@ EMAIL_PORT = 587
 
 ADMIN_EMAIL = 'wikibreach2017@gmail.com'
 CONTACT_EMAIL = 'aditisharma.b@gmail.com'
+SITE_URL = 'https://localhost:8000'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- send_alerts.py
+++ send_alerts.py
@@ -1,7 +1,7 @@
 # __author__ = "Aditi Sharma"
 
 from django.core.mail import EmailMultiAlternatives
-from WikiBreach.settings import ADMIN_EMAIL
+from WikiBreach.settings import ADMIN_EMAIL, SITE_URL
 from subscription.models import SubscribedUser
 
 
@@ -9,7 +9,7 @@ def send_email(title, slug):
     list = SubscribedUser.get_emails()
     for email in list:
         print("Sending email to: " + email)
-        content = "https://localhost:8000/posts/"+slug
+        content = SITE_URL + "/posts/"+slug
         subject, from_email, to = 'WikiBreach Alert', ADMIN_EMAIL, email
         text_content = "New breach has been reported"
         html_content = '<strong>A new breach has been reported </strong><a href='+'\"'+ content +'\"'+'target=\"_blank\"><h1>' + title + '</h1></a><br> <p> Visit wikibreach today to view all the details </p>'
